cd114b9933cf64aed711f26085d634359dbb4dca
Shorecut <chermanderco@gmail.com>
2023-07-23 16:56:58 +0600

    add dome

--- src/routes/MainRoute.jsx
+++ src/routes/MainRoute.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import HomePage from "../pages/HomePage";
+import AddPinPage from "../pages/AddPinPage";
 
 const MainRoute = () => {
   return (
@@ -9,6 +10,7 @@ const MainRoute = () => {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
+          <Route path="/add" element={<AddPinPage />} />
         </Route>
       </Routes>
     </div>
