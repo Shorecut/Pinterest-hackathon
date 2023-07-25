dfb38aa8732f94cb6eda49a8025e1138a2a84e0f
edilbek1507 <edilbek555@gmail.com>
2023-07-25 16:13:58 +0600

    cards and navbar

--- src/components/PinItem.jsx
+++ src/components/PinItem.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import "../card.css";
-
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 const PinItem = ({ item, pinSize }) => {
   return (
     <div className={`pin ${pinSize}`}>
       <img src={item.image} alt="" className="mainPic" />
-
+      <div className="card_cover"></div>
+      <div className="content2">
+        <button className="card_btn_save">Сохранить</button>
+      </div>
       <div className="content">
-        <h3>{item.title}</h3>
-        <div className="search">
-          <a>
-            <img
-              src="https://www.svgrepo.com/show/533627/arrow-square-right.svg"
-              alt=""
-            />
-          </a>
-        </div>
+        <button className="card_button">
+          <svg
+            className="card_deatils bi bi-three-dots"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <EditIcon />
+          </svg>
+        </button>
+        <button className="card_button card_button_item2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-three-dots card_deatils"
+            viewBox="0 0 16 16"
+          >
+            <DeleteIcon />
+          </svg>
+        </button>
       </div>
     </div>
   );
